43489a5aa1cbaa214774d058b5e979d9573ca80a
duybaodn06 <duybaodn06@gmail.com>
2026-08-22 14:49:34 +0700

    upload section 3

---
+++ section2/student/student/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1.Create an array of at least 5 Student objects with different names and scores.
+        Student s1 = new Student("Nguyen Van A", 9.5);
+        Student s2 = new Student("Tran Thi B", 4.0);
+        Student s3 = new Student("Tran Van C", 5.0);
+        Student s4 = new Student("Tang Thi D", 10.0);
+        Student s5 = new Student("Le Van E", 3.5);
+
+        //2.Print the total number of students created (using the static method).
+        Console.WriteLine(Student.GetTotalStudents());
+
+        //3.Print the list of students along with their classification and pass/ fail status(using instance methods).
+        Student[] listStudent = { s1, s2, s3, s4, s5 };
+        Console.WriteLine("List classified students");
+        foreach (Student student in listStudent)
+        {
+            Console.WriteLine($"Name: {student.GetName()}, Classification: {student.GetClassification()}, Status: {student.IsPassed()}");
+        }
+
+        //4.Print the top-scoring student (using the static method).
+        Student topStudent = Student.FindTopStudent(listStudent);
+        Console.WriteLine($"Top student: Name: {topStudent.GetName()}, Score: {topStudent.GetScore()}");
+
+        //5.Print the class average score (using the static method).
+        Console.WriteLine($"Average score: {Student.CalculateAverageScore(listStudent)}");
+    }
+}
---
+++ section3/UserAccountManagementSystem/UserAccountManagementSystem/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class UserAccount
+{
+    private string _password;
+    private decimal _balance;
+
+    public string AccountId { get; init; }
+
+    public string Username { get; set; }
+
+    public string Password { set { _password = "[ENCRYPTED]_" + value; } }
+
+    public decimal Balance
+    {
+        get { return _balance; }
+        set
+        {
+            if (value >= 0) _balance = value;
+            else Console.WriteLine("Error: Balance cannot be negative!");
+        }
+    }
+
+    public bool IsVIP => Balance >= 10000m;
+    public DateTime CreatedDate { get; }
+
+    // Constructor
+    public UserAccount()
+    {
+        // TODO: Initialize CreatedDate here
+        CreatedDate = DateTime.Now;
+    }
+}
+
